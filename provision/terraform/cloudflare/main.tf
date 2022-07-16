f08235171e9f416e73489c75c2ed417af2d48cf8
Jason White <jw2dotorg@gmail.com>
2022-07-16 13:34:07 -0400

    Sync provision and tmpl tasks

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -38,7 +38,7 @@ resource "cloudflare_zone_settings_override" "cloudflare_settings" {
     ssl = "strict"
     # /ssl-tls/edge-certificates
     always_use_https         = "on"
-    min_tls_version          = "1.0"
+    min_tls_version          = "1.2"
     opportunistic_encryption = "on"
     tls_1_3                  = "zrt"
     automatic_https_rewrites = "on"
@@ -85,23 +85,14 @@ data "http" "ipv4" {
 resource "cloudflare_record" "ipv4" {
   name    = "ipv4"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = chomp(data.http.ipv4.body)
+  value   = chomp(data.http.ipv4.response_body)
   proxied = true
   type    = "A"
   ttl     = 1
 }
 
-#resource "cloudflare_record" "root" {
-#  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
-#  zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-#  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
-#  proxied = true
-#  type    = "CNAME"
-#  ttl     = 1
-#}
-
-resource "cloudflare_record" "hajimari" {
-  name    = "hajimari"
+resource "cloudflare_record" "root" {
+  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
